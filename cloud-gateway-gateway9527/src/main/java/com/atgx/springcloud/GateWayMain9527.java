c9ad257170838b85c01c576c1f80cc9a1b46902e
liugx <sunshine_lgx@163.com>
2023-04-23 09:10:23 +0800

    Predicate的使用;自定义全局GlobalFilter

---
+++ cloud-gateway-gateway9527/src/main/java/com/atgx/springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.atgx.springcloud;
+
+import cn.hutool.core.map.multi.AbsTable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: liugx
+ * @CreateTime: 2023-04-18  08:39
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/atgx/springcloud/config/MyLogGateWayFilter.java
@@ -0,0 +1,46 @@
+package com.atgx.springcloud.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @Author: liugx
+ * @CreateTime: 2023-04-23  08:35
+ * @Description: 自定义 过滤器 filter
+ * @Version: 1.0
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
+    {
+        log.info("***********come in MyLogGateWayFilter:  "+new Date());
+
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+
+        if(uname == null)
+        {
+            log.info("*******用户名为null，非法用户，o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder()
+    {
+        return 0;
+    }
+}
+
