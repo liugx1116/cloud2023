53b4dbc2019a183797be2c722ea6ee9114539858
liugx <sunshine_lgx@163.com>
2023-06-26 08:44:21 +0800

    SpringCloud Bus消息总线

---
+++ cloud-config-client-3366/src/main/java/com/atgx/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.atgx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: liugx
+ * @CreateTime: 2023-06-25  08:49
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
