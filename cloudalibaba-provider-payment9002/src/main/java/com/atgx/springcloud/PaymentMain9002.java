33b6cd54d250b9bbb0c6962799685458d0822235
liugx <sunshine_lgx@163.com>
2023-07-13 08:49:46 +0800

    SpringCloud Alibaba Nacos创建服务提供者9002

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atgx/springcloud/PaymentMain9002.java
@@ -0,0 +1,19 @@
+package com.atgx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: liugx
+ * @CreateTime: 2023-07-12  08:53
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
