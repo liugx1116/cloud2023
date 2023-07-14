e1f096e3cd1c4cd5518042a31951ce7c8c5eeea7
liugx <sunshine_lgx@163.com>
2023-07-14 14:46:30 +0800

    Nacos作为配置中心-基础配置

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atgx/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package com.atgx.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: liugx
+ * @CreateTime: 2023-07-14  09:00
+ * @Description: nacos 配置中心启动类
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
